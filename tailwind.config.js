269fb5297a28ab49ea8efd3d6da00aa8e2eca3b7
kingxalpha <tosinsirmuel@gmail.com>
2023-09-29 12:16:12 +0100

    landing and about page done

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'navy-blue': '#045089',
+        'Ash': '#F9F9F9',
+        'blaq': '#121212',
+      },
+    },
+  },
+  plugins: [],
+}
+
